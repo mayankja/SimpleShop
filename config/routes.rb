357aac3e1218e210f73fd0dd6db13b4c4a34e3a0
mayankja <mayankjadam74@gmail.com>
2021-09-09 13:06:33 +0530

    Added routes for devise as well as for others

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  #devise_for :users
+  namespace :api, defaults: { format: :json } do
+    post 'users/login', to: 'users#login'
+    post 'sign_up', to: 'users#create', as: :sign_up
+    resources :regions
+    resources :products
+    resources :orders
+    resources :order_items
+  end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
