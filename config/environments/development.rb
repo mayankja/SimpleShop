704074c707c781f5a917803a4d5527dd6e39960b
mayankja <mayankjadam74@gmail.com>
2021-09-09 13:30:09 +0530

    Added mailer funcnality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+    user_name:       Rails.application.secrets[:user_name],
+    password:       Rails.application.secrets[:password],
+    address:       Rails.application.secrets[:address],
+    domain:        Rails.application.secrets[:domain],
+    port:          Rails.application.secrets[:port],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
